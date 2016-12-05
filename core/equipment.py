c72686e7a414e2e1204a4a013992172a93248cd1
Loikki <loic_hausammann@hotmail.com>
2016-12-05 16:31:12 +0100

    test + description equipment

--- core/equipment.py
+++ core/equipment.py
@@ -59,17 +59,6 @@ class EquipmentParser():
             return None
         description = "<p><b><u>" + str(name) + "</u></b></p>\n"
 
-        if child.tag == 'spell':
-            description += "<p><b> Level </b>" + str(child.get('level')) + " "
-        else:
-            description += "<p><b> Cantrip </b> "
-        description += child.get('type') + "</p>" + "\n"
-
-        description += "<p><b> Casting Time </b>" + str(child.get('casting_time')) + "</p>\n"
-        description += "<p><b> Range </b>" + str(child.get('range')) + "</p>\n"
-        description += "<p><b> Components </b>" + str(child.get('component')) + "</p>\n"
-        description += "<p><b> Duration </b>" + str(child.get('duration')) + "</p>\n"
-
         temp = ET.tostring(child.find('description'))
         ind_open = temp.index("<description>")
         ind_close = temp.index("</description>")
